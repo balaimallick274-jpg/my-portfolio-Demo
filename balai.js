fafc4b0fa3a533f253a5be1944e62aa5a2ece2e2
Balai mallick <balaimallick274@gmail.com>
2026-04-18 19:56:12 +0530

    first commit

---
+++ balai.js
@@ -0,0 +1,38 @@
+var tablinks = document.getElementsByClassName("tab-links");
+var tabcontents = document.getElementsByClassName("tab-content");
+
+function opentab(tabname, event) {
+  for (let tablink of tablinks) {
+    tablink.classList.remove("active-link");
+  }
+  for (let tabcontent of tabcontents) {
+    tabcontent.classList.remove("active-tab");
+  }
+  event.currentTarget.classList.add("active-link");
+  document.getElementById(tabname).classList.add("active-tab");
+}
+var sidemenu = document.getElementById("sidemenu");
+function openmenu() {
+  sidemenu.style.right = "0";
+}
+function closemenu() {
+  sidemenu.style.right = "-200px";
+}
+
+const scriptURL =
+  "https://script.google.com/macros/s/AKfycbxOf1kLw6Z3t0LCBtL-cIjbWwP3NJc0Lr2WDw99cRWZYvUq_c-ESEf0Bd409eRULiQX/exec";
+const form = document.forms["submit-to-google-sheet"];
+const msg = document.getElementById("msg");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  fetch(scriptURL, { method: "POST", body: new FormData(form) })
+    .then((response) => {
+      msg.innerHTML = "Message sent Succrssfully";
+      setTimeout(function () {
+        msg.innerHTML = "";
+      }, 2000);
+      form.reset();
+    })
+    .catch((error) => console.error("Error!", error.message));
+});
